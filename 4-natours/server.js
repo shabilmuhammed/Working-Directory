436c4e713d400b44f33f93d39533e4f89392fc2e
Muhammed Shabil <NA2634755@hrblock.com>
2024-01-30 20:52:58 +0530

    Tour  Model Updates

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -7,17 +7,26 @@ dotenv.config({
   path: `${__dirname}/config.env`,
 });
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD,
-);
+let DB = '';
 
+// Update at the end of the course
+if (process.version === 'v18.12.1') {
+  DB = process.env.DATABASE_HRB.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD,
+  );
+} else {
+  DB = process.env.DATABASE_PERSONAL.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD,
+  );
+}
 mongoose
   .connect(DB, {
-    // useNewUrlParser: true,
-    //useFindAndModify: false,
-    //useCreateIndex: true,
-    // useUnifiedTopology: true,
+    //useNewUrlParser: true,
+    // useFindAndModify: false,
+    // useCreateIndex: true,
+    //useUnifiedTopology: true,
   })
   .then(() => {
     console.log('Connected to MongoDB');
@@ -43,5 +52,6 @@ mongoose
 //console.log(app.get('env'));
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
+  console.log('Node version', process.version);
   console.log(`Server running on port ${port}`);
 });
